97afc638e8c4cb26814d7e1d504f1b81ecf965a8
Sami Fink <fink.s.m.8651@gmail.com>
2024-04-25 10:15:35 +0000

    feat: added bodyparser

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -20,11 +23,14 @@ app.get('/api/hello', function(req, res) {
 });
 
 // URL shortener endpoint
-app.route('/api/shorturl/').post(function (req, res) {
+app.route('/api/shorturl/:short-url').post(function (req, res) {
   // output
   res.json({
     original_url: "placeholder",
     short_url: "placeholder"
+  })
+  .get(function (req, res) {
+
   })
 });
 
